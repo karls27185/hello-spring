7f96bfec6b44ab3879c7f796cf2ddef42145a33a
karls27185 <karls27185@gmail.com>
2023-04-27 00:35:42 +0300

    end

--- src/main/java/com/javastart/hellospring/service/AccountService.java
+++ src/main/java/com/javastart/hellospring/service/AccountService.java
@@ -5,6 +5,7 @@ import com.javastart.hellospring.exception.AccountNotFoundException;
 import com.javastart.hellospring.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,14 +19,23 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
 
+    private final AccountService2 accountService2;
+
     @Autowired
-    public AccountService(AccountRepository accountRepository) {
+    public AccountService(AccountRepository accountRepository, AccountService2 accountService2) {
         this.accountRepository = accountRepository;
+        this.accountService2 = accountService2;
     }
 
+    @Transactional
     public Long createAccount(String name, String email, Integer bill) {
         Account account = new Account(name, email, bill);
-        return accountRepository.save(account).getId();
+        Account account1 = accountRepository.save(account);
+        accountService2.saveAccount2();
+        int a = 1 / 0;
+        Account account2 = new Account("Account2", "martin@cat.xyz", 1000);
+        accountRepository.save(account2);
+        return account1.getId();
     }
 
     public Account getAccountById(Long id) {
@@ -33,11 +43,11 @@ public class AccountService {
                 new AccountNotFoundException("Can't find account with id: " + id));
     }
 
-    public List<Account> getAll(){
+    public List<Account> getAll() {
         return accountRepository.findAll();
     }
 
-    public Account deleteById(Long id){
+    public Account deleteById(Long id) {
         Account account = getAccountById(id);
         accountRepository.deleteById(id);
         return account;
---
+++ src/main/java/com/javastart/hellospring/service/AccountService2.java
@@ -0,0 +1,33 @@
+package com.javastart.hellospring.service;
+
+import com.javastart.hellospring.entity.Account;
+import com.javastart.hellospring.exception.AccountNotFoundException;
+import com.javastart.hellospring.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author Karl
+ * @link <a href="https://babayan.keenetic.link/">https://babayan.keenetic.link</a>
+ */
+
+@Service
+public class AccountService2 {
+
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService2(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void saveAccount2(){
+        Account account3 = new Account("Account3", "email", 4000);
+        accountRepository.save(account3);
+    }
+}
